8aac5ce0cfcee2c17b8c09e88a90ded65948964c
Dragos Calin <calinho_gx1992@yahoo.com>
2014-05-20 12:09:02 +0300

    Install Devise

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Smartalbum::Application.config.secret_key_base = '1ec897376ce6ae7d118be8d6cd49c40d1805df807e14823af89b30f5a391b37384402a86bec040fbf48014519915174b4e94ff4b315a8f8724569d36b3c36536'
