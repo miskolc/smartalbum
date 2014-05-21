1a7883575173eeca936aa9a820b1f4cfa69132d9
Dragos Calin <calinho_gx1992@yahoo.com>
2014-05-21 20:01:14 +0300

    Add thumbnails to images listings

--- app/uploaders/store_uploader.rb
+++ app/uploaders/store_uploader.rb
@@ -36,6 +36,10 @@ class StoreUploader < CarrierWave::Uploader::Base
     process :resize_to_fit => [1500, 500]
   end
 
+  version :thumb do
+    process :resize_to_fit => [300,200]
+  end  
+
   # Add a white list of extensions which are allowed to be uploaded.
   # For images you might use something like this:
   # def extension_white_list
