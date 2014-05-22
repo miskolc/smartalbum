a3bb05bf98cb30d3c240581850ae861887afab43
Dragos Calin <calinho_gx1992@yahoo.com>
2014-05-22 14:31:01 +0300

    Get width for uploaded image

--- app/workers/image_data_miner.rb
+++ app/workers/image_data_miner.rb
@@ -1,7 +1,13 @@
+require 'exifr'
+
 class ImageDataMiner
   include Sidekiq::Worker
+  sidekiq_options retry: false
 
-  def perform(name, count)
-    logger.info 'Doing hard work'
+  def perform(image, count)
+    sleep 10
+    exposure_time = EXIFR::JPEG.new(image).exposure_time.to_s
+    width = EXIFR::JPEG.new(image).width.to_s
+    logger.info('Doing hard work?' + exposure_time + ' ' + width)
   end
 end
